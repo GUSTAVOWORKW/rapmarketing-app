bdf1ae6c5f77c28f5e918ffbec264126d66763a2
pedrohgl18 <pedrohgl18@gmail.com>
2025-06-27 00:43:37 -0300

    duplo scroll criar smart link

--- src/pages/CreateSmartLinkPage.tsx
+++ src/pages/CreateSmartLinkPage.tsx
@@ -347,7 +347,7 @@ const CreateSmartLinkPage: React.FC = () => {
     );
   }
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50">
+    <div className="bg-gradient-to-br from-gray-50 to-blue-50">
       <div className="container mx-auto p-4 max-w-7xl">
         {notification && (
           <div 
